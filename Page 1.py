fca3c80e75f55e976c474633d01c8a7a2d3d5444
Nashalan <sivalingamnashalan@gmail.com>
2025-11-02 19:52:07 +0800

    Update Page 1.py

--- Page 1.py
+++ Page 1.py
@@ -20,6 +20,7 @@ def load_data():
     df = pd.read_csv(DATA_URL)
     df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
     return df
+df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
 
 df = load_data()
 
